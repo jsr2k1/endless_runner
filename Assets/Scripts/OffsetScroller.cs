55826cf5cf36ac65d79be3595725dc3cc9e962f0
aratingastudios <aratingadevelopers@gmail.com>
2015-07-22 13:57:56 +0200

    Se ha añadido un poco de todo

---
+++ Assets/Scripts/OffsetScroller.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OffsetScroller : MonoBehaviour
+{
+	public float scrollSpeed;
+	Vector2 savedOffset;
+	float mult = 0.1f;
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	void Start()
+	{
+		savedOffset = renderer.sharedMaterial.GetTextureOffset("_MainTex");
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	void Update()
+	{
+		float x = Mathf.Repeat(Time.time * scrollSpeed * mult, 1);
+		Vector2 offset = new Vector2(x, savedOffset.y);
+		renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	void OnDisable()
+	{
+		renderer.sharedMaterial.SetTextureOffset("_MainTex", savedOffset);
+	}
+}
