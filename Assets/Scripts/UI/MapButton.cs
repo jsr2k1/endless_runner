9dc5b35304b73add53528e1f3373215406775b3d
aratingastudios <aratingadevelopers@gmail.com>
2015-07-27 12:32:41 +0200

    Escena de mapa

---
+++ Assets/Scripts/UI/MapButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class MapButton : MonoBehaviour, IPointerClickHandler
+{
+	public void OnPointerClick(PointerEventData data)
+	{
+		string num_level = name.Split('_')[1];
+		Application.LoadLevel("Level_" + num_level);
+	}
+}
